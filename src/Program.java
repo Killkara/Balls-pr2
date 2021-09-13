f8bcd289e1a5467e7d07871af27858b956812570
Killkara <killkara21@gmail.com>
2021-09-13 13:05:19 +0300

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,8 @@
+public class Program {
+    public static void main(String[] args) {
+        Ball a=new Ball(10,10);
+        System.out.println(a);
+        a.move(10,10);
+        System.out.println(a);
+    }
+}
